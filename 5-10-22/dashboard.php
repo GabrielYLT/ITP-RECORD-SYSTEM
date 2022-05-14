657eaa36b7b525284de0d185861dc032c8b0fb7e
GabrielYLT <90229401+GabrielYLT@users.noreply.github.com>
2022-05-14 10:14:13 +0800

    Update dashboard.php

--- 5-10-22/dashboard.php
+++ 5-10-22/dashboard.php
@@ -87,14 +87,15 @@ $(document).ready(function(){
             <a>
               <i class="now-ui-icons design_bullet-list-67"></i>
               <h3 class="dropdown-header" style="color:white;">Category</h3>
-			  <a class="dropdown-item" style="width:80%;margin-left:13%;" href="list.php?details&id=1"><i class="now-ui-icons design_palette"></i>New Year Cookies</a>
-			  <a class="dropdown-item" style="width:80%;margin-left:13%;" href="list.php?details&id=2	"><i class="now-ui-icons design_palette"></i>Raya Cookies</a>
-			  <a class="dropdown-item" style="width:80%;margin-left:13%;" href="list.php?details&id=3"><i class="now-ui-icons design_palette"></i>Mooncakes</a>
-			   <a class="dropdown-item" style="width:80%;margin-left:13%;" href="list.php?details&id=4"><i class="now-ui-icons design_palette"></i>Raw Material</a>
-			  <a class="dropdown-item" style="width:80%;margin-left:13%;" href="list.php?details&id=5"><i class="now-ui-icons design_palette"></i>Packing Material</a>
-			  <a class="dropdown-item" style="width:80%;height:10%;margin-left:13%;" href="list.php?details&id=6"><i class="now-ui-icons design_palette"></i>General Use</a>
-			  
 			</a>
+			  <ul class="nav" style="padding">
+			  <li ><a class="dropdown-item" style="width:80%;margin-left:13%;" href="#"><i class="now-ui-icons design_palette"></i>New Year Cookies</a></li>
+			  <li><a class="dropdown-item" style="width:80%;margin-left:13%;" href="#"><i class="now-ui-icons design_palette"></i>Raya Cookies</a></li>
+			  <li><a class="dropdown-item" style="width:80%;margin-left:13%;" href="#"><i class="now-ui-icons design_palette"></i>Mooncakes</a></li>
+			  <li><a class="dropdown-item" style="width:80%;margin-left:13%;" href="#"><i class="now-ui-icons design_palette"></i>Packing Material</a></li>
+			  <li><a class="dropdown-item" style="width:80%;margin-left:13%;" href="#"><i class="now-ui-icons design_palette"></i>Raw Material</a></li>
+			  <li><a class="dropdown-item" style="width:80%;height:10%;margin-left:13%;" href="#"><i class="now-ui-icons design_palette"></i>General Use</a></li>
+			  </ul>
           </li>
           <li>
             <a href="#">
