56f24aefccc988b1db2ea31f75461d5bc228c544
Chan-125 <76606779+Chan-125@users.noreply.github.com>
2022-05-23 14:32:15 +0800

    Update test.php

--- 5-10-22/test.php
+++ 5-10-22/test.php
@@ -1,50 +1,289 @@
 <?php
+include("Connection.php");
 session_start();
-include_once 'Connection.php';
-if(isset($_POST['submit']))
-{
-    $user_id = $_POST['user_id'];
-    $result = mysqli_query($connect,"SELECT * FROM admin where AID='" . $_POST['user_id'] . "'");
-    $row = mysqli_fetch_assoc($result);
-	$fetch_user_id=$row['AID'];
-	$email_id=$row['AEmail'];
-	$password=$row['APassword'];
-	if($user_id==$fetch_user_id) {
-				$to = $email_id;
-                $subject = "Password";
-                $txt = "Your password is : $password.";
-                $headers = "From: smtp.gmail.com" . "\r\n" .
-                "CC: somebodyelse@example.com";
-                mail($to,$subject,$txt,$headers);
-			}
-				else{
-					echo 'invalid userid';
-				}
-}
+error_reporting(0);
 ?>
-<!DOCTYPE HTML>
-<html>
-<head>
-<style type="text/css">
- input{
- border:1px solid olive;
- border-radius:5px;
- }
- h1{
-  color:darkgreen;
-  font-size:22px;
-  text-align:center;
- }
+<?php
+if(!isset($_SESSION['id']))
+{
+?>
+    <script>
+    alert("Please login. Thank you!!!");
+    </script>
+    <?php
+    header("refresh:0.001;url=login.php");
+    //exit();
+	
+	
+}else{
+	$Admin_id=$_SESSION['id'];
+$result=mysqli_query($connect,"SELECT *FROM admin WHERE AID = $Admin_id");
+$row = mysqli_fetch_assoc($result);
+	if(($row['Department'])!='All Department')
+{	
+?>
+    <script>
+    alert("You Are Not Authorize To Access This Page!!!");
+    </script>
+    <?php
+    header("refresh:0.001;url=generalD.php");
+    //exit();
+	
+}
+}
 
+if(isset($_GET["details"])){
+$ad_id=$_GET['id'];	
+$result=mysqli_query($connect,"SELECT * FROM category WHERE CID='$ad_id'");
+$row=mysqli_fetch_assoc($result);}
+?>
+<!DOCTYPE html>
+<html>
+	<head>
+	<meta charset="UTF-8">
+	<title><?php echo $row["CName"]." List" ?></title>
+	<link rel="icon" type="image/x-icon" href="images/icons/d.png">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	
+		<!-- CSS Files -->
+		<link href="assets/css/bootstrap.min.css" rel="stylesheet" />
+		<link href="assets/css/now-ui-dashboard.css" rel="stylesheet" />
+		<!-- CSS Files -->
+  <link id="pagestyle" href="assets/css/argon-dashboard.css" rel="stylesheet" />
+		<!-- https://fontawesome.com/ -->
+		<link rel="stylesheet" href="assets/css/bootstrap.css">
+		<!-- https://getbootstrap.com/ -->
+		<link rel="stylesheet" href="css/tooplate.css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+<script>
+$(document).ready(function(){
+  $("#myInput").on("keyup", function() {
+    var value = $(this).val().toLowerCase();
+    $("#myTable tr").filter(function() {
+      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+    });
+  });
+});
+</script>
+<style>
+/* width */
+::-webkit-scrollbar {
+  width: 5px;
+}
+
+/* Track */
+::-webkit-scrollbar-track {
+  box-shadow: inset 0 0 2px grey; 
+  border-radius: 5px;
+}
+ 
+/* Handle */
+::-webkit-scrollbar-thumb {
+  background: grey; 
+  border-radius: 10px;
+}
+
+/* Handle on hover */
+::-webkit-scrollbar-thumb:hover {
+  background: #fff; 
+}
+
+label {
+	color:black;
+}
 </style>
-</head>
-<body>
-<h1>Forgot Password<h1>
-<form action='' method='post'>
-<table cellspacing='5' align='center'>
-<tr><td>user id:</td><td><input type='text' name='user_id'/></td></tr>
-<tr><td></td><td><input type='submit' name='submit' value='Submit'/></td></tr>
-</table>
-</form>
-</body>
+		</head>
+	
+	<body>
+<div class="wrapper"  style="overflow:hidden;height:100%" >
+    <div class="sidebar" data-color="pink">
+	
+      <!--
+        Tip 1: You can change the color of the sidebar using: data-color="blue | green | orange | red | yellow"
+    -->
+      <div class="logo">
+        <a href="dashboard.php" class="simple-text logo-mini">
+          JMM
+        </a>
+        <a href="dashboard.php" class="simple-text logo-normal">
+          RECORD SYSTEM
+        </a>
+      </div>
+      <div class="sidebar-wrapper" id="sidebar-wrapper" >
+        <ul class="nav">
+          <li>
+            <a href="dashboard.php">
+              <i class="now-ui-icons education_paper"></i>
+              <p>Dashboard</p>
+            </a>
+          </li>
+          <li>
+            <a>
+              <i class="now-ui-icons design_bullet-list-67"></i>
+              <h3 class="dropdown-header" style="color:white;">Category</h3>
+			</a>
+			<?php 
+			
+			$conn = $connect;
+			
+			if ($conn->connect_error) {
+			die("Connection failed: " . $conn->connect_error);
+			}
+			$sql3 = "SELECT * FROM category ";
+			$result3 = $conn->query($sql3);
+			if ($result3->num_rows > 0) {
+			while($row3 = $result3->fetch_assoc()) {
+				
+			echo "<ul class='nav' style='padding'>";
+			echo "<li><a class='dropdown-item' style='width:80%;margin-left:13%;margin-top:-7%;' href='list.php?details&id=". $row3["CID"]."'><i class='now-ui-icons design_palette'></i>". $row3["CName"]. "</a></li></ul> </li>";
+			
+			}
+			} 
+			?>
+          </li>
+		   <li>
+            <a href="admin_list.php">
+              <i class="now-ui-icons users_circle-08"></i>
+              <p>Admin List</p>
+            </a>
+          </li>
+          <li>
+            <a href="manage.php">
+              <i class="now-ui-icons loader_gear"></i>
+              <p>Manage</p>
+            </a>
+          </li>
+        </ul>
+	  </div>
+    </div>
+    <div class="main-panel" id="main-panel" style="height:100%;background-image: url('images/bg.jpg');background-size: cover;">
+		<nav class="navbar navbar-expand-lg navbar-transparent  bg-primary  navbar-absolute" style="opacity:1;">
+			<div class="container-fluid">
+			    <div class="navbar-wrapper">
+					<div class="navbar-toggle">
+					    <button type="button" class="navbar-toggler">
+							<span class="navbar-toggler-bar bar1"></span>
+							<span class="navbar-toggler-bar bar2"></span>
+							<span class="navbar-toggler-bar bar3"></span>
+					    </button>
+					</div>
+			    </div>
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navigation" aria-controls="navigation-index" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-bar navbar-kebab"></span>
+					<span class="navbar-toggler-bar navbar-kebab"></span>
+					<span class="navbar-toggler-bar navbar-kebab"></span>
+				</button>
+				<div class="collapse navbar-collapse justify-content-end" id="navigation">
+					<ul class="navbar-nav">
+					    <li class="nav-item">
+						  <Tooltip title="Profile">
+							<a class="nav-link" href="viewProfile.php">
+							    <i class="now-ui-icons users_single-02"></i>
+							    <p>
+									<span class="d-lg-none d-md-block">Profile</span>
+							    </p>
+							</a>
+						  </Tooltip>
+					    </li>
+						<li class="nav-item">
+						  <Tooltip title="Logout">
+							<a class="nav-link" href="logout.php">
+							    <i class="now-ui-icons media-1_button-power"></i>
+							    <p>
+									<span class="d-lg-none d-md-block">Logout</span>
+							    </p>
+							</a>
+						  </Tooltip>
+					    </li>
+					</ul>
+			    </div>
+			</div>
+        </nav>
+		<div><br><br><br></div>
+			<!-- row -->
+            <div class="row tm-content-row tm-mt-big" style="font-family: 'Lato', sans-serif;padding-left:1%;padding-top:3%;padding-right:1%;padding-bottom:1%;">
+                <div class="col-xl-20 col-lg-12 tm-md-12 tm-sm-12 tm-col" style="padding-top:auto%;margin: auto; margin-bottom:2%;">
+                    <div class="tm-block h-100" style="border-radius:10px;border-style: groove;background-color: #ffffff;opacity: 75%;">
+                        <div class="row">
+                            <div class="col-md-8 col-sm-12">
+							<?php
+							if(isset($_GET["details"])){
+								
+							$ad_id=$_GET['id'];	
+							$result=mysqli_query($connect,"SELECT * FROM category WHERE CID='$ad_id'");
+							$row=mysqli_fetch_assoc($result);}
+									
+									
+							echo "<h2 class='tm-block-title d-inline-block' style='margin-left:3%;margin-top:2%;color:black;font-weight:bold;'>List of ". $row["CName"] . " </h2>";
+							?>
+                            </div>
+							<div>
+							<hr>
+
+<h6 style="margin-left:auto%;margin-top:auto%;"class="text-white text-capitalize ps-3"><input style="width:99%;height:35px;margin-left:autos%;border-radius:10px;border-style: none;" autocomplete=off id="myInput" type="text" name="searchname" placeholder="Search" ></h6>
+<hr>
+</div>
+                        </div>
+                        <div class="table-responsive">
+                            <table class="table table-hover table-striped tm-table-striped-even mt-3">
+                                <thead>
+                                    <tr class="tm-bg-gray">
+                                        <th scope="col" style="text-align:center;color:black;font-weight:bold;">Code</th>
+										<th scope="col" style="text-align:center;color:black;font-weight:bold;">Image</th>
+                                        <th scope="col" style="text-align:center;color:black;font-weight:bold;">Name</th>
+										<th scope="col" style="text-align:center;color:black;font-weight:bold;">Quantity</th>
+										<th scope="col" style="text-align:center;color:black;font-weight:bold;">Added By</th>
+										<th scope="col" style="width:20%">&nbsp;</th>
+                                    </tr>
+                                </thead>
+                                <tbody id="myTable">
+                                	<?php
+									$conn = $connect;
+									if(isset($_GET["details"])){
+								
+									$ad_id=$_GET['id'];
+
+										
+									if ($conn->connect_error) {
+									die("Connection failed: " . $conn->connect_error);
+									}
+									$sql = "SELECT product.PCode, product.PImage, product.PName,product.PQty,product.QType,product.CID,category.CName,product.AID,admin.AName
+									FROM ((product INNER JOIN category ON product.CID = category.CID)INNER JOIN admin ON product.AID = admin.AID)
+									WHERE product.CID = '$ad_id'  ";
+									$result = $conn->query($sql);
+									if ($result->num_rows > 0) {
+
+									while($row = $result->fetch_assoc()) {
+									echo "<tr><td style='text-align:center;color:black;font-weight:bold;'>" . $row["PCode"] . "</td>" ;
+									echo "<td style='text-align:center;color:black;font-weight:bold;'> <img width='125px' src='images/" . $row["PImage"]. "'></td>" ; 	
+									echo "<td style='text-align:center;color:black;font-weight:bold;'>" . $row["PName"].  "</td>" ; 
+									echo "<td style='text-align:center;color:black;font-weight:bold;'>" . $row["PQty"]."&nbsp;&nbsp;". $row["QType"] ."</td>" ; 
+									echo "<td style='text-align:center;color:black;font-weight:bold;'>" . $row["AName"].  "</td>" ; 
+                                    ?> 
+                                  <td style="text-align:center;font-weight:bold";>
+									<div class='btn-group'> 
+									<a href="details.php?details&id=<?php echo $row['PCode'];?>" class="btn btn-secondary">Details</a>	
+									<a href="editProduct.php?details&id=<?php echo $row['PCode'];?>" class="btn btn-secondary" >Edit</a></td>
+									</div>
+                                    <?php
+									echo "</tr>" ;
+									}
+									echo "</table>";
+									} else { echo "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;No results Found !"; }}
+									?>    
+                                </tbody>
+                            </table>
+                        </div>
+            </div>
+        </div>
+    </div>
+		
+		
+
+  </div>
+	<script src="assets/js/core/jquery.min.js"></script>
+	<script src="assets/js/core/bootstrap.min.js"></script>
+	<script src="assets/js/plugins/perfect-scrollbar.jquery.min.js"></script>
+	<script src="assets/js/now-ui-dashboard.min.js" type="text/javascript"></script>
+	</body>
 </html>
