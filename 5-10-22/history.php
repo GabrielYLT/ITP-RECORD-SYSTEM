9b0e2d176be65556ecdf7b44c9f4e4c620bce2ce
GabrielYLT <90229401+GabrielYLT@users.noreply.github.com>
2022-05-24 17:21:15 +0800

    Update history.php

--- 5-10-22/history.php
+++ 5-10-22/history.php
@@ -60,20 +60,11 @@ $row=mysqli_fetch_assoc($result);}
 		<link rel="stylesheet" href="assets/css/bootstrap.css">
 		<!-- https://getbootstrap.com/ -->
 		<link rel="stylesheet" href="css/tooplate.css">
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+
 		
  
  
-<script>
-$(document).ready(function(){
-  $("#myInput").on("keyup", function() {
-    var value = $(this).val().toLowerCase();
-    $("#myTable tr").filter(function() {
-      $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
-    });
-  });
-});
-</script>
+
 <style>
 /* width */
 ::-webkit-scrollbar {
