bf50af9d47eb8286c5edef50dfb8907664660956
GabrielYLT <90229401+GabrielYLT@users.noreply.github.com>
2022-05-25 11:02:27 +0800

    Update fetch.php

--- 5-10-22/fetch.php
+++ 5-10-22/fetch.php
@@ -21,14 +21,14 @@ $search = str_replace(",", "|", $_POST["query"]);
  OR admin.AName REGEXP '".$search."' 
 OR stock.Remarks REGEXP '".$search."' 
  OR stock.Status REGEXP '".$search."'
- group BY PCode,AID,Status ORDER BY DateAdded
+ group BY PCode,AID,Status,DATE(DateAdded) ORDER BY DateAdded
  ";
 }
 else
 {
  $query = "SELECT stock.PCode,product.PName,product.PImage,product.QType,product.CID,category.CName,SUM(stock.Qty) AS total_qty,stock.AID,admin.AName,stock.DateAdded,stock.Remarks,stock.Status
 									FROM (((stock INNER JOIN product ON stock.PCode = product.PCode)INNER JOIN category ON product.CID = category.CID)INNER JOIN admin ON stock.AID= admin.AID)
-									group BY PCode,AID,Status ORDER BY DateAdded DESC
+									group BY PCode,AID,Status,DATE(DateAdded) ORDER BY DateAdded DESC
 
  ";
 }
